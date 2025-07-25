da248c28088f6c12ad3956a0b6c55bf4cf33e023
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-25 09:19:26 -0500

    Update tokenList.ts

--- frontend/lib/tokenList.ts
+++ frontend/lib/tokenList.ts
@@ -1,55 +1,38 @@
 // lib/tokenList.ts
-
-export interface TokenInfo {
-  name: string;
-  symbol: string;
-  address: string;
-  logoURI: string;
-  decimals: number;
-}
-
-export const tokenList: TokenInfo[] = [
+const tokenList = [
   {
     name: 'Toby',
     symbol: '$TOBY',
     address: '0x29cbAa230EE61767fa0020d5B6bDC88643cBcd2d',
-    logoURI: '/logos/toby.png',
+    logoURI: 'https://assets.coingecko.com/coins/images/34853/standard/to.png', // sample logo
     decimals: 18,
+    coingeckoId: 'toby',
   },
   {
     name: 'Patience',
     symbol: '$PATIENCE',
     address: '0x6D96f18F00B815B2109A3766E79F6A7aD7785624',
-    logoURI: '/logos/patience.png',
+    logoURI: 'https://assets.coingecko.com/coins/images/34854/standard/pa.png',
     decimals: 18,
+    coingeckoId: 'patience',
   },
   {
     name: 'Taboshi',
     symbol: '$TABOSHI',
-    address: '0x82303bF85872f8E4058eD06dD3c6588E60cBB7fC',
-    logoURI: '/logos/taboshi.png',
+    address: '0x3c836B6Ae06D802b1A301E224A04eB937A1f898B',
+    logoURI: 'https://assets.coingecko.com/coins/images/34855/standard/ta.png',
     decimals: 18,
+    coingeckoId: 'taboshi',
   },
   {
-    name: 'USDC',
-    symbol: 'USDC',
-    address: '0xd9AA2824d79F584aDCc0f1FA1cE823c3Cb0e9600',
-    logoURI: 'https://assets.coingecko.com/coins/images/6319/small/usdc.png',
-    decimals: 6,
-  },
-  {
-    name: 'WETH',
-    symbol: 'WETH',
-    address: '0x4200000000000000000000000000000000000006',
-    logoURI: 'https://assets.coingecko.com/coins/images/2518/small/weth.png',
-    decimals: 18,
-  },
-  {
-    name: 'ZORA',
+    name: 'Zora',
     symbol: 'ZORA',
-    address: '0x5a7D6B55Af2032dE75a1bA9e7c661D6f1bBfE5cE',
-    logoURI: 'https://assets.coingecko.com/coins/images/31537/small/zora_logo.jpg',
+    address: '0x7A1f0F71dF4e8A3d9BfBf8b31e7b0C1dbE6e3D2A',
+    logoURI: 'https://assets.coingecko.com/coins/images/34485/standard/zora.png',
     decimals: 18,
+    coingeckoId: 'zora',
   },
-  // Add more Base/Farcaster tokens here as needed
+  // Add more tokens as needed...
 ];
+
+export default tokenList;
