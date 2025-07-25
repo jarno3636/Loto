e93b6c8d397b2b50c103fd31551066fe9df38b9a
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-25 17:08:30 -0500

    Add files via upload

---
+++ frontend/next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/frame",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
