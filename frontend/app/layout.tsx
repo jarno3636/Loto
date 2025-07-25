036565913a8e23ca068cf174c2bf8be69b6cd696
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-25 08:54:38 -0500

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.PNG" />
       </head>
       <body className="bg-slate-950 text-white font-sans">{children}</body>
     </html>
