256c976de3ed356b675fbc6aa951913eaf895e71
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-25 22:00:18 -0500

    Update tokenList.ts

--- frontend/lib/tokenList.ts
+++ frontend/lib/tokenList.ts
@@ -1,45 +1,48 @@
+// frontend/lib/tokenList.ts
+
 export interface TokenInfo {
-  address: string;
-  symbol: string;
   name: string;
+  symbol: string;
+  address: string;
   decimals: number;
   logoURI: string;
 }
 
-export const tokenList = [
+export const tokenList: TokenInfo[] = [
   {
     name: 'Toby',
-    symbol: '$TOBY',
+    symbol: 'TOBY',
     address: '0xbcAD0a417b299f611f386e9ab38A049E06494C0c',
-    logoURI: 'https://toadgod.xyz/toby-logo.png',
     decimals: 18,
+    logoURI: 'https://toadgod.xyz/images/toby.png',
   },
   {
     name: 'Patience',
-    symbol: '$PATIENCE',
+    symbol: 'PATIENCE',
     address: '0x6d96f18f00b815b2109a3766e79f6a7ad7785624',
-    logoURI: 'https://toadgod.xyz/patience-logo.png',
     decimals: 18,
+    logoURI: 'https://toadgod.xyz/images/patience.png',
   },
   {
     name: 'Taboshi',
-    symbol: '$TABOSHI',
+    symbol: 'TABOSHI',
     address: '0x3a1a33cf4553db61f0db2c1e1721cd480b02789f',
-    logoURI: 'https://toadgod.xyz/taboshi-logo.png',
     decimals: 18,
+    logoURI: 'https://toadgod.xyz/images/taboshi.png',
   },
   {
     name: 'LOTO',
-    symbol: '$LOTO',
+    symbol: 'LOTO',
     address: '0x615346aD915D6592d1961a141a8670D698e3BbE7',
-    logoURI: '/loto.PNG',
     decimals: 18,
+    logoURI: '/loto.PNG', // Case-sensitive, make sure your file matches
   },
   {
     name: 'Zora',
-    symbol: '$ZORA',
-    address: '0x501b2a56dd25d2dfada3e4f4fb020da2d8a9fe8d',
-    logoURI: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x501B2A56Dd25D2DfADA3E4f4Fb020Da2d8a9fE8D/logo.png',
+    symbol: 'ZORA',
+    address: '0x7e123C5E6B1C936eDDC79A4197f72e9763Bc32B2',
     decimals: 18,
-  }
+    logoURI: 'https://assets.coingecko.com/coins/images/30123/small/zora.jpg',
+  },
+  // Add more as needed!
 ];
