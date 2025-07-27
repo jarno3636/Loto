9e84865d7202a08d9a3611b98bb143a68c87425b
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-26 22:12:25 -0500

    Create WalletConnectButton.tsx

---
+++ frontend/components/WalletConnectButton.tsx
@@ -0,0 +1,8 @@
+// components/WalletConnectButton.tsx
+export default function WalletConnectButton() {
+  return (
+    <button className="bg-gradient-to-r from-blue-600 to-violet-600 px-4 py-2 rounded-lg font-bold text-white shadow">
+      Connect Wallet
+    </button>
+  );
+}
